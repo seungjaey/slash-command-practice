ebb59b1f921dbf0149691536fb63fe8165084276
hamtolchu <seungjaey@gmail.com>
2024-02-29 20:35:52 +0900

    [TEST] PR 정보 조회 응답

--- src/pages/api/command.ts
+++ src/pages/api/command.ts
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+import { Octokit, App } from "octokit";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { method, headers, body } = req;
   if (method !== "POST") {
     return res.status(404);
@@ -25,22 +27,61 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   response_url: 'https://hooks.slack.com/commands/T3ZPFBBPV/6745235981312/CYUSYadzyJ5gC5Er1w5pTRnK',
   trigger_id: '6724902147332.135797385811.d3ac7b93fb6b823b5f805c1b055a01f6' */
   const { text } = body;
+
+  // 1. text -> PR 번호가 숫자로 변환 가능한지 검수
+  // 2. PR 번호로 kurlymall-nx 에서 조회
+  // 3. 조회된 PR 정보에서 제목과 링크를 추출하여 메세지 구성
+  // TODO: 한번 생성된 메세지에 인터렉티브하게 사용자 입력을 받거나 업데이트 하려면 어떻게 해야할지 ?
+
+  // TODO: Github 인증에 실패한 경우
+  const octokit = new Octokit({
+    auth: process.env.GITHUB_PAT,
+  });
+
+  // TODO: text 숫자 변환에 실패한 경우 처리
+  const pullRequest = await octokit.request(
+    "GET /repos/{owner}/{repo}/pulls/{pull_number}",
+    {
+      owner: "thefarmersfront",
+      repo: "kurlymall-nx",
+      pull_number: Number(text),
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    },
+  );
+
+  const {
+    data: { title, html_url },
+  } = pullRequest;
+
+  // NOTE: 리뷰 상태에 따른 업데이트
+
   res.status(200).send({
     blocks: [
       {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: `요청 - ${text}`,
+          text: `📢 동네사람덜 리뷰해주세유 📢 `,
         },
       },
       {
         type: "section",
         text: {
           type: "mrkdwn",
-          text: "왕 하면 왕 짖는 멍뭉이",
+          text: `[${title}](${html_url})`,
+        },
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `🦮 왕 하면 왕 대답함 🐕‍🦺 : ${text}`,
         },
       },
     ],
   });
-}
+};
+
+export default handler;
